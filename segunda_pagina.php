9366fe32c363b6ab5e209ed7120269e4b80e54e3
hernandezaw <128613758+hernandez-alex@users.noreply.github.com>
2023-06-10 09:20:23 -0400

    v04 variables y comentarios

--- segunda_pagina.php
+++ segunda_pagina.php
@@ -7,10 +7,34 @@
 </head>
 <body>
   <?php
+    // Este es un comentario de una línea
+    /*
+    Este es un comentario 
+    multilínea
+    */
+
+    // Declaración de variables
     $nombre = "Juan";
     $edad = 18;
-    print $nombre;
-    print $edad;
+
+    // Concatenación usando comillas dobles y punto
+    echo "El nombre del usuario es " . $nombre;
+    echo "<br/>";
+
+    // Concatenación interpolando comillas dobles
+    echo "El nombre del usuario es $nombre";
+    echo "<br/>";
+
+    // Concatenación interpolando comillas simples ERROR
+    echo 'El nombre del usuario es $nombre';
+    echo "<br/>";
+
+    // Diferencias estre la función print y la expresión echo
+    // echo admite la impresioń multiple
+    echo $nombre, $edad;
+    echo "<br/>";
+    // print no admite la impresión multiple
+    // print $nombre, $edad; // ERROR
   ?>
 </body>
 </html>
