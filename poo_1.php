422d41cc71d55cc89671885a914f47d30f07d14f
alex <62507090+hernandezaw@users.noreply.github.com>
2023-06-13 20:46:40 -0400

    v23 poo 2

--- poo_1.php
+++ poo_1.php
@@ -9,12 +9,39 @@
   <?php
     // Creando una clase
     class Coche{
+      // Creando propiedades (caracteristicas) a una clase
+      var $ruedas;
+      var $color;
+      var $motor;
 
+      // Constructor __construct(): indica el estado inicial de los objetos que pertenecen
+      // a la clase
+      function __construct(){
+        // Antiguamente era una función con el mismo nombre de la clase
+        $this->ruedas= 4;
+        $this->color= "";
+        $this->motor= 1600;
+      }
+
+      // Creando métodos (funcionalidades)
+      function arrancar(){
+        echo "Estoy arrancando <br/>";
+      }
+      function girar(){
+        echo "Estoy girando <br/>";
+      }
+      function frenar(){
+        echo "Estoy frenando <br/>";
+      }
     }
 
     // Creando una instancia de la clase Coche
     $renault = new Coche();
     $mazda = new Coche();
+    $seat = new Coche();
+
+    $mazda->girar();
+    echo $mazda->ruedas;
   ?>
 </body>
 </html>
