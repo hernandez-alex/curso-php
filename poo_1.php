4eb0a16bac770a43d4b9b9ec1d61ff0c07ba4972
alex <62507090+hernandezaw@users.noreply.github.com>
2023-06-13 20:18:58 -0400

    POO I

---
+++ poo_1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>POO I</title>
+</head>
+<body>
+  <?php
+    // Creando una clase
+    class Coche{
+
+    }
+
+    // Creando una instancia de la clase Coche
+    $renault = new Coche();
+    $mazda = new Coche();
+  ?>
+</body>
+</html>
