3eb7331adad91066f4b35a27527d2acb0fd31671
hernandezaw <128613758+hernandez-alex@users.noreply.github.com>
2023-07-14 16:52:21 -0400

    temporal video 30

---
+++ array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Arrays en PHP</title>
+</head>
+<body>
+  <?php
+    $semana[] = "Lunes";
+    $semana[] = "Martes";
+    $semana[] = "Miercoles";
+
+    echo $semana[2];
+  ?>
+</body>
+</html>
